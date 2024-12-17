acb4f8ee096793a996d3ea7c277a24e24ef11ad8
Bavley Mobarak <142764224+bavleym@users.noreply.github.com>
2024-12-16 21:01:28 -0800

    Update Onnx Test.py

--- YOLO Model Final Version/Onnx Test.py
+++ YOLO Model Final Version/Onnx Test.py
@@ -1,5 +1,5 @@
 import onnx
-model = onnx.load(r"C:\Users\Gaiaf\OneDrive - csulb\Documents\CECS 490 B\best.onnx")
+model = onnx.load(r"best.onnx")
 print("Model Inputs:")
 for input in model.graph.input:
     print(input.name)
